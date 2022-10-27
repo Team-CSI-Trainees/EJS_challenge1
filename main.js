7eee3ac87240daf4094b548145ae383540abb2cb
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-10-28 04:40:07 +0530

    Challenge-11

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const ejs = require("ejs");
 const homeStartingContent ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 const aboutContent = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const contactContent = "lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McCli";
-
+const composeTitle ="";
+const composePost = "";
 const app =express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
@@ -22,6 +23,16 @@ res.render("about",{aboutContent:aboutContent})
 app.get("/contact",function(req,res){
 res.render("contact",{contactContent:contactContent})
 })
+app.get("/compose",function(req,res){
+res.render("compose",{})
+});
+app.post("/compose",function(req,res){
+ let post={composeTitle:req.body.composeTitle,
+   composePost:req.body.composePost
+
+   }
+ console.log(post);
+});
 
   
 
