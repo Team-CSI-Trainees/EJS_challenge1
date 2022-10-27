e4e41231afc0392fbebee5d258ba81df0c57d7cb
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-10-28 01:05:27 +0530

    6 challenge complete

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const homeStartingContent ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 const aboutContent = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const contactcontent = "lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McCli";
+const contactContent = "lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McCli";
 
 const app =express();
 app.set("view engine","ejs");
@@ -14,8 +14,14 @@ app.get("/",function(req,res){
     const home = "HOME";
  res.render("home",{
     HOME:home, Content:homeStartingContent
- });
+ });  
 });
+app.get("/about",function(req,res){
+res.render("about",{aboutContent:aboutContent})
+})
+app.get("/contact",function(req,res){
+res.render("contact",{contactContent:contactContent})
+})
 
   
 
