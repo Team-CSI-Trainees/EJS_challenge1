ce2dd40a1b5635759a68affbf0077202c5ab796b
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-05 17:21:13 +0530

    try-3

--- index.js
+++
@@ -1,4 +0,0 @@
-const mongoose =require('mongoose');
-mongoose.connect("mongodb://localhost:27017/blogDB1 ",{useNewUrlParser:true}).then(()=>
-console.log("connection successfull"))
-.catch((err) => console.log(err) );
