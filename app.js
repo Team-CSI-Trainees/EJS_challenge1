feb828b358ae4333af28b4f0881f96e118777b45
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2022-11-05 16:43:16 +0530

    Deployment

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
-const str = require('@supercharge/strings')
+const str = require('@supercharge/strings');
 const mongoose =require('mongoose');
 const { urlencoded } = require("body-parser");
+const e = require("express");
+const port =process.env.PORT || 3000;
 // const blog = require("../blogWebsite/model/data");                                                                                          
 mongoose.connect("mongodb://localhost:27017/blogDB ",{useNewUrlParser:true}).then(()=>
 console.log("connection successfull"))
@@ -21,11 +23,15 @@ const blogSchema = new mongoose.Schema({
     default:Date.now
   }
   });
+  const titleSchema = new mongoose.Schema({
+    blogTitle:String
+  })
   const Post =  mongoose.model("Post",blogSchema);
+  //model to be stored in post 
+
+   const ReadPost =  mongoose.model("ReadPost",titleSchema);
 
 
- let Blogposts = [];
-//model to be stored in post 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -48,16 +54,12 @@ app.get("/", function(req, res){
       console.log(err);
     }
     else{
-    // console.log(posts);
-    //   console.log(blogContent);
+
     res.render("home", {
       startingContent:homeStartingContent,
        posts: posts});
     }
   });
-  
-
-
 });
 
 app.get("/about", function(req, res){
@@ -70,7 +72,6 @@ app.get("/contact", function(req, res){
 
 app.get("/compose", function(req, res){
   res.render("compose");
-  // console.log(blogContent);
 });
 
 app.post("/compose", function(req,res){
@@ -80,86 +81,40 @@ app.post("/compose", function(req,res){
      blogPost: req.body.postBody,
      limit:str(req.body.postBody).limit(100,"...").get()
    });
-   Blogposts.push(post.content);
-  //  console.log(str(req.body.postBody).limit(100,"...").get());
-   post.save(function(err){
+    console.log(Post.findOne({},{content:1}));
+   
+   const readPost = new ReadPost({
+       blogTitle:post.content
+   });
+       post.save(function(err){
     if(err){
       console.log(err);
     }
     else{
       res.redirect("/")
-    }
-   });
-  //  var blogdataTitle =  (req.body.postTitle);
-  //  var blogdataPost =  req.body.postBody;
-  //  const postData = DataBlog({
-  //   content:{BlogdataTitle},
-  //   blogPost:{BlogdataPost}
-  //  });
-   
-  //  posts.push(post);
-
-  //  DataBlog.insertMany({postData},function(err){
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   else{ 
-  //     console.log("Database Connected");
-  //   }
-  // });
-  //  postData.save()
-  //  console.log(postData);
-  // DataBlog.find({},function(err,DataBlogSaved){
-  //   if(err)
-  //   {
-  //     console.log(err);
-  //   }
-  //   else{
-  //        console.log(DataBlogSaved);
-  //   }
-
-  // })
-  
-
-  //  res.redirect("/");
-   
+    }});
 });
 
 
 
 
 app.get("/posts/:Name", function(req, res){
-  const blogCont =blogContent;
   const displayTitle =req.params.Name;
   const matchTitle = _.lowerCase(req.params.Name);
-  console.log(Blogposts);
-
-//   Post.findOne({_id:matchTitle},function(err,post)
-//    {
-//     res.render("post", {
-//       title:displayTitle
-//       ,bPost:blogCont});
-//   });
-// });
-Blogposts.forEach(function(e){
-  if(_.lowerCase(e) === matchTitle)
-  {     res.render("post", {
-         title:e
-         ,bPost:blogCont});}
-  else{
-    res.render("404");
-  }
+  
+Post.find({}, function(err, result) {
+  if (err) throw err;
+  result.forEach(element => {
+    console.log(element.content);
+    if(_.lowerCase(element.content) === matchTitle)
+    {res.render("post", {
+      title:element.content
+      ,bPost:element.blogPost});}
+      else res.render ("404");
+  });
+  }); 
 });
-});
-// {if(_.lowerCase(blogCont)===matchTitle){
-//   res.render("post", {
-//     title:displayTitle
-//     ,bpost:blogCont});
-//     console.log(e.cTitle);
-//     console.log(e.cPost);
-// }
-// }});
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started");
 });
